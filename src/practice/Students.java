f075cbefb96dfc0c5de2fd34aaa2fa51a1ca1331
Faizan Tarique <faizant@mindfiresolutions.com>
2021-10-21 21:33:11 +0530

    Modified print statemant

--- src/practice/Students.java
+++ src/practice/Students.java
@@ -3,7 +3,7 @@ package practice;
 public class Students {
 
 	public static void main(String[] args) {
-		System.out.println("Here my students are learning Git + GitHub");
+		System.out.println("Here my students are learning Git + GitHub. Faiza is teaching");
 
 	}
 
