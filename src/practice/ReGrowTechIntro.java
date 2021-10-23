ac85b5bcb31ed95250d56f56fd4a9e339314a723
Tejasavi <sonitejasavi1@gmail.com>
2021-10-23 11:27:48 +0530

    Testing

--- src/practice/ReGrowTechIntro.java
+++ src/practice/ReGrowTechIntro.java
@@ -3,7 +3,7 @@ package practice;
 public class ReGrowTechIntro {
 
 	public static void main(String[] args) {
-		System.out.println("Hi We are studying at ReGrow Tech");
+		System.out.println("Hi We are studying at ReGrow Tech. We r learning how to implement git hub");
 
 	}
 
