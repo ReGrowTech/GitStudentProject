1d7b4bef772e2bdb17b4c6dfeec98621e38ef3de
shanawaz ahmad <shanawazahmad8092@gmail.com>
2021-11-06 16:39:00 +0530

    changes is been done

--- src/practice/ReGrowTechIntro.java
+++ src/practice/ReGrowTechIntro.java
@@ -4,6 +4,7 @@ public class ReGrowTechIntro {
 
 	public static void main(String[] args) {
 		System.out.println("Hi We are studying at ReGrow Tech september batch");
+		System.out.println("Its Alreadt november");
 
 	}
 
